902a483559f2c37d7bb60d20e181255c3c419b62
monatcan <monatcan@gmail.com>
2026-02-10 12:31:25 +0300

    fix: revert ecosystem config to use next start

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,9 @@ module.exports = {
   apps: [
     {
       name: 'bakim-app',
-      script: '.next/standalone/server.js',
-      cwd: './', // Run from root so it can find .env and potentially other resources
-      node_args: '-r dotenv/config', // Ensure .env is loaded if standalone doesn't do it automatically
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start',
+      cwd: './',
       env: {
         NODE_ENV: 'production',
         PORT: 3000,

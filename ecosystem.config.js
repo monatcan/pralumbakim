d68aa5e18f0b42f6af8c1a305bae5fcb2f12221a
monatcan <monatcan@gmail.com>
2026-02-10 12:34:42 +0300

    fix: configure iis to passthrough application errors and bind next.js to all interfaces

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       env: {
         NODE_ENV: 'production',
         PORT: 3000,
-        HOSTNAME: '127.0.0.1',
+        HOSTNAME: '0.0.0.0',
         NEXTAUTH_URL: 'https://gemtech.net.tr', // Public URL
       },
     },
